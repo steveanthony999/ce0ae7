198f5b6cdac39a650ce10a5ede938eea0212a973
Steven Woodward <steveanthony999@gmail.com>
2022-04-20 12:54:37 -0700

    bug fixes for displaying messages on state update and in the correct order

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,23 @@
-const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  {
+    logging: false,
+    ssl: process.env.DB_ENABLE_SSL,
+    dialectOptions:
+      process.env.DB_ENABLE_SSL === 'true'
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+  }
+);
 
 module.exports = db;
